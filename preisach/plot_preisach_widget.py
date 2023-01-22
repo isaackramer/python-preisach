897531904374624c481a0ffabfa2653fec2b4c72
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-01-22 08:55:43 +0200

    Paint it black, and some manual formatting on weights

--- preisach/plot_preisach_widget.py
+++ preisach/plot_preisach_widget.py
@@ -15,19 +15,21 @@ from .weights import weights
 hys_per_side = 101
 
 # create meshgrid with location of hysterons
-beta_values = np.around(np.linspace(0, 1, hys_per_side),2) # beta values
-alpha_values = np.around(np.linspace(1,0, hys_per_side),2) # alpha values
+beta_values = np.around(np.linspace(0, 1, hys_per_side), 2)
+alpha_values = np.around(np.linspace(1, 0, hys_per_side), 2)
 beta_grid, alpha_grid = np.meshgrid(beta_values, alpha_values)
 
 # create array showing whether hysterons on/off
-# inital values: all hysterons set to 0 (negative saturation) and values outside of preisach triangle set to np.nan
-preisach_triangle = np.where(alpha_grid>=beta_grid, 0, np.nan)
+# inital values: all hysterons set to 0 (negative saturation) and
+# values outside of preisach triangle set to np.nan
+preisach_triangle = np.where(alpha_grid >= beta_grid, 0, np.nan)
 
-# assign weights: "uniform", "linear", "bottom_heavy", "top_heavy", "left_heavy", "right_heavy"
-# "center_light_alpha", "center_light_beta", "center_heavy_alpha", "center_heavy_beta"
+# assign weights: "uniform", "linear", "bottom_heavy", "top_heavy",
+# "left_heavy", "right_heavy" "center_light_alpha", "center_light_beta",
+# "center_heavy_alpha", "center_heavy_beta"
 mu = weights("irreversible", beta_grid, alpha_grid)
 mass = np.nansum(mu)
-mu = mu/mass
+mu = mu / mass
 
 
 # initial state of system
@@ -42,19 +44,18 @@ plt.clf()
 
 fig = plt.figure(1)
 gs = gridspec.GridSpec(2, 2, width_ratios=[2, 1], height_ratios=[1, 1])
-gs.update(left=0.1, right = 0.95, bottom=0.25, top = 0.9, wspace = 0.3)
+gs.update(left=0.1, right=0.95, bottom=0.25, top=0.9, wspace=0.3)
 
 
-ax1 = plt.subplot(gs[:, 0]) # Input/output
-ax2 = plt.subplot(gs[0, 1]) # Preisach plane
-ax3 = plt.subplot(gs[1, 1]) # Weights
+ax1 = plt.subplot(gs[:, 0])  # Input/output
+ax2 = plt.subplot(gs[0, 1])  # Preisach plane
+ax3 = plt.subplot(gs[1, 1])  # Weights
 
-plot1, = ax1.plot(inputs[0], 1 - outputs[0], color="red")
-plot2 = ax2.pcolormesh(beta_grid, alpha_grid, preisach_triangle,
-                       cmap = YlGnBu_9.mpl_colormap)
-plot3 = ax3.scatter(beta_grid, alpha_grid, c = mu,
-                    cmap = YlGnBu_9.mpl_colormap,
-                    s = 0.2)
+(plot1,) = ax1.plot(inputs[0], 1 - outputs[0], color="red")
+plot2 = ax2.pcolormesh(
+    beta_grid, alpha_grid, preisach_triangle, cmap=YlGnBu_9.mpl_colormap
+)
+plot3 = ax3.scatter(beta_grid, alpha_grid, c=mu, cmap=YlGnBu_9.mpl_colormap, s=0.2)
 
 
 # axis limits
@@ -64,20 +65,23 @@ ax2.set_xlim([-0.05, 1.05])
 ax2.set_ylim([-0.05, 1.05])
 
 # add slider (rect = [left, bottom, width, height])
-axcolor = 'lightgoldenrodyellow'
+axcolor = "lightgoldenrodyellow"
 axinputs = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
-sinputs = Slider(axinputs, 'input', valmin = 0, valmax = 1, valinit=0.0)
+sinputs = Slider(axinputs, "input", valmin=0, valmax=1, valinit=0.0)
 
 # function to update plots based on slider input
 def update(val):
     global inputs, outputs, preisach_triangle
     inputs = np.concatenate((inputs, np.array([sinputs.val])))
-    outputs, preisach_triangle = preisach_single_value(inputs, alpha_grid, beta_grid, mu,
-                                                       preisach_triangle, outputs)
+    outputs, preisach_triangle = preisach_single_value(
+        inputs, alpha_grid, beta_grid, mu, preisach_triangle, outputs
+    )
     plot1.set_ydata(outputs)
     plot1.set_xdata(inputs)
     ax2.pcolormesh(beta_grid, alpha_grid, preisach_triangle)
     fig.canvas.draw_idle()
 
-# for the slider to remain responsive you must maintain a reference to it. Call on_changed() to connect to the slider event.
+
+# for the slider to remain responsive you must maintain a reference to it.
+# Call on_changed() to connect to the slider event.
 sinputs.on_changed(update)
--- preisach/preisach_python.py
+++ preisach/preisach_python.py
@@ -2,42 +2,48 @@
 
 import numpy as np
 
-def preisach_single_value(inputs, alpha_grid, beta_grid, mu, preisach_triangle, outputs):
+
+def preisach_single_value(
+    inputs, alpha_grid, beta_grid, mu, preisach_triangle, outputs
+):
     """Preisach function with single input"""
 
     # compare new input to previous input value and change hysteron values accordingly
-    if inputs[-1] > inputs[-2]: # if input increases
-        preisach_triangle = np.where(inputs[-1]>alpha_grid, 1, preisach_triangle)
-    elif inputs[-1] < inputs[-2]: # if input increases
-        preisach_triangle = np.where(inputs[-1]<beta_grid, 0, preisach_triangle)
+    if inputs[-1] > inputs[-2]:  # if input increases
+        preisach_triangle = np.where(inputs[-1] > alpha_grid, 1, preisach_triangle)
+    elif inputs[-1] < inputs[-2]:  # if input increases
+        preisach_triangle = np.where(inputs[-1] < beta_grid, 0, preisach_triangle)
 
     # values outside the presiach half-plane are set to nan
-    preisach_triangle = np.where(alpha_grid>=beta_grid, preisach_triangle, np.nan)
+    preisach_triangle = np.where(alpha_grid >= beta_grid, preisach_triangle, np.nan)
 
     # calculate weighted presiach triangle
-    weighted_preisach = preisach_triangle*mu
+    weighted_preisach = preisach_triangle * mu
 
     # new output value
     f = np.nansum(weighted_preisach)
     outputs = np.concatenate((outputs, np.array([f])))
     return outputs, preisach_triangle
 
+
 def preisach_array(inputs, alpha_grid, beta_grid, mu, starting_value):
     """Preisach function for array of input values"""
 
-    preisach_triangle = np.where(alpha_grid>=beta_grid, starting_value, np.nan)
-    outputs = np.array([np.nansum(preisach_triangle*mu)])
+    preisach_triangle = np.where(alpha_grid >= beta_grid, starting_value, np.nan)
+    outputs = np.array([np.nansum(preisach_triangle * mu)])
 
-    for ii in range(len(inputs)-1):
-        new_input = inputs[ii+1]
+    for ii in range(len(inputs) - 1):
+        new_input = inputs[ii + 1]
         old_input = inputs[ii]
-        if new_input > old_input: # if input increases
-            preisach_triangle = np.where(new_input>alpha_grid, 1, preisach_triangle)
-        elif new_input < old_input: # if input increases
-            preisach_triangle = np.where(new_input<beta_grid, 0, preisach_triangle)
 
-        preisach_triangle = np.where(alpha_grid>=beta_grid, preisach_triangle, np.nan)
-        weighted_preisach = preisach_triangle*mu
+        if new_input > old_input:  # if input increases
+            preisach_triangle = np.where(new_input > alpha_grid, 1, preisach_triangle)
+        elif new_input < old_input:  # if input increases
+            preisach_triangle = np.where(new_input < beta_grid, 0, preisach_triangle)
+
+        preisach_triangle = np.where(alpha_grid >= beta_grid, preisach_triangle, np.nan)
+        weighted_preisach = preisach_triangle * mu
+
         f = np.nansum(weighted_preisach)
         outputs = np.concatenate((outputs, np.array([f])))
 
